0bab66862af28e08661beb188e8121b13e2fb6bd
Jakob <jakob_gauffin@hotmail.com>
2020-10-31 02:21:54 +0100

    Add simple markup and styling

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
-        <p>Greetings from footer.php</p>
+        <footer class="Footer">
+            <div class="Footer__Content"></div>
+                    
+                <div class="Footer__Bottom"></div>
+        </footer>
     <?php wp_footer(); ?> 
     </body>
 </html>   
