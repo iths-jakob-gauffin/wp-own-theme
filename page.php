9dbcaee81486422e8a6b9799b2b0464ec98d034c
Jakob <jakob_gauffin@hotmail.com>
2020-10-31 02:24:57 +0100

    Add bgimage to page and more

--- page.php
+++ page.php
@@ -4,9 +4,18 @@
 
     while(have_posts()){
         the_post(); ?>
-            <h1>This is a page not a post</h1> 
-            <h2><?php the_title() ?></h2>
-            <p><?php the_content()?></p>
+            <main class="Page">
+
+                <div class="Page__BackgroundImage" style="background-image: url(<?php echo get_theme_file_uri( '/images/guychickdog.webp' )?>); background-position: 50% 25%; background-size: cover;">
+
+                    <div class="Page__BackgroundOverlay"></div>
+                    
+                        <div class="Page__Wrapper">
+                            <h2 class="Page__Title"><?php the_title() ?></h2>
+                            <div class="Page__Content"><?php echo the_content(); ?></div>
+                        </div>
+                    </div>
+            </main>
         <?php
     }
 
