87ffdae9eb6e15ba1a9720cc07b96508ffdc305f
Jakob <jakob_gauffin@hotmail.com>
2020-10-31 02:23:33 +0100

    Add routing with site_url

--- header.php
+++ header.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <!-- <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+        
         <?php wp_head(); ?>
     </head>
     <body>
+        <!-- <div class="container"> -->
         <header class="Header">
-            <h1 class="Header__Title">Eget temasajt</h1>
-            <nav class="Header__Nav">
-                <ul class="Header__NavList">
-                    <li class="Header__ListItem">
-                        <a href="#" class="Header__Link">Home</a>
-                    </li>
-                    <li class="Header__ListItem">
-                        <a href="#" class="Header__Link">Courses</a>
-                    </li>
-                    <li class="Header__ListItem">
-                        <a href="#" class="Header__Link">Blog</a>
-                    </li>
-                    <li class="Header__ListItem">
-                        <a href="#" class="Header__Link">About Us</a>
-                    </li>
-                    <li class="Header__ListItem">
-                        <a href="#" class="Header__Link">Contact Us</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="Header__ButtonRoundWrapper">
-                <button class="ButtonRound">Be A Contributor</button>
-                <button class="ButtonRound">Login</button>
-            </div>
-        </header>
+                <h1 class="Header__Title"><a href="<?php echo site_url('/'); ?>">Eget temasajt</a></h1>
+                <nav class="Header__Nav">
+                    <ul class="Header__NavList">
+                        <li class="Header__ListItem">
+                            <a href="<?php echo site_url('/'); ?>" class="Header__Link">Home</a>
+                        </li>
+                        <li class="Header__ListItem">
+                            <a href="#" class="Header__Link">Courses</a>
+                        </li>
+                        <li class="Header__ListItem">
+                            <a href="#" class="Header__Link">Blog</a>
+                        </li>
+                        <li class="Header__ListItem">
+                            <a href="<?php echo site_url('/about-us')?> " class="Header__Link">About Us</a>
+                        </li>
+                        <li class="Header__ListItem">
+                            <a href="#" class="Header__Link">Contact Us</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="Header__ButtonRoundWrapper">
+                    <button class="ButtonRound">Be A Contributor</button>
+                    <button class="ButtonRound">Login</button>
+                </div>
+            </header>
+        <!-- </div> -->
